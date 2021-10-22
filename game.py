8b329432f0bdc71238d469ac6f7efcc17265491e
sullentrash <72536372+sullentrash@users.noreply.github.com>
2021-10-22 15:46:21 +0300

    Add files via upload

---
+++ game.py
@@ -0,0 +1,12 @@
+﻿import pygame
+
+class Game():
+    def __init__(self):
+        pygame.init()
+        self.running, self.playing = True, False
+        self.DISPLAY_W, self.DISPALY_H = 720, 480
+        self.display = pygame.Surface((self.DISPLAY_W, self.DISPLAY_W))
+        self.window = pygame.display.set_mode((self.DISPLAY_W, self.DISPLAY_W))
+        self.font_name = '8-BIT WONDER.TTF'
+        #self.font_name = pygame.font.get_default_font()
+        self.BLACK, self.WHITE = (0, 0, 0), (255, 255, 255)
