305b4af8e9a92943cd4fe6ad0e5e0d54c03a2a75
sullentrash <72536372+sullentrash@users.noreply.github.com>
2021-11-14 13:25:55 +0500

    Класс комнаты и веб-хук телеги
    room.py - содержит класс комнаты для хранения пользователей
    WebHook.py - содержит телеграмм бота для общения клиент-сервер(образно говоря)

---
+++ WebHook.py
@@ -0,0 +1,133 @@
+﻿from room import Room
+import telebot
+#from telebot import type
+
+# Bot TOKEN
+TOKEN = '1757432372:AAHNMbgLfYR6Yb4nR76cAY67Voju8MGTzpQ'
+
+#Список комнат
+knownRooms = []
+
+#Доступные команды
+commands = {
+    'create_room'       : 'Create new room',
+    'join_room'         : 'Join existing room',
+    'exit_room'         : 'Exit current room',
+	'rooms'	            : 'Show amount of the rooms and amount of users in each one',
+    'send_ans'          : 'Send answer to current question',
+    'help'              : 'Show available commands'
+}
+
+bot = telebot.TeleBot(TOKEN) #Создание бота
+
+def check_user(id):
+    """
+    Проверка состоит ли пользователь в какой-либо комнате,
+    Если да - вернуть её номер
+    Если нет - вернуть -1
+    """
+    f = -1
+    n = len(knownRooms)
+    for i in range(n):
+        m = knownRooms[i].count()
+        for j in range(m):
+            if knownRooms[i].Users[j] == id:
+                f = i
+                break
+    return f
+
+
+@bot.message_handler(commands=['create_room'])
+def command_create_room(m):
+    """
+    Обработка команды создания комнаты, её очищение, присваивание UID
+    """
+    id = m.from_user.id
+    buf = Room()
+    #Заглушка, создание UID для комнаты на основе порядка её создания в боте
+    buf.setUID(len(knownRooms) + 1)
+    bot.send_message(id, 'Room created, Room UID: {}'.format(buf.UID))
+    knownRooms.append(buf)
+
+
+@bot.message_handler(commands=['join_room'])
+def command_join_room(m):
+    """
+    Обработка команды присоединения пользователя к комнате
+    """
+    id = m.from_user.id
+    #Проверить не находится ли пользователь уже в какой-либо комнате
+    check1 = check_user(id)
+    if check1 != -1:
+        bot.send_message(id, 'You are already in the room {}'.format(check1))
+        return
+
+    mes_text = m.text[10:]
+    numBuf = int(mes_text)
+    #Добавить обработку ошибки при неправильном номере комнаты!!!!
+    n = len(knownRooms)
+    check2 = True
+    for i in range(n):
+        if knownRooms[i].UID == numBuf:
+            knownRooms[i].insert(id)
+            check2 = False
+    if check2:
+        bot.send_message(id, 'Room with UID {}, does not exist'.format(numBuf))
+        return
+    bot.send_message(id, 'You successfully joined Room: {}, your ID: {}'.format(numBuf, id))
+
+
+@bot.message_handler(commands=['exit_room'])
+def command_exit_room(m):
+    """
+    Обработка команды выхода из комнаты
+    """
+    id = m.from_user.id
+    #Проверить не находится ли пользователь уже в какой-либо комнате
+    check1 = check_user(id)
+    if check1 == -1:
+        bot.send_message(id, 'You havent entered the Room yet')
+        return
+
+    mes_text = m.text[10:]
+    numBuf = int(mes_text)
+    #Добавить обработку ошибки при неправильном номере комнаты!!!!
+    knownRooms[check1].remove(id)
+    bot.send_message(id, 'You successfully exited Room: {}, your ID: {}'.format(numBuf, id))  
+
+@bot.message_handler(commands=['rooms'])
+def command_rooms(m):
+    """
+    Обработка команды демонстрации информации о комнатах
+    """
+    id = m.from_user.id
+    n = len(knownRooms)
+    mes_text = 'There are total: {} Rooms now\n'.format(n)
+    for i in range(n):
+        buf = 'Room: {}, Users: {}\n'.format(knownRooms[i].UID, knownRooms[i].count())
+        mes_text += buf
+    bot.send_message(id, mes_text)
+
+@bot.message_handler(commands=['send_ans'])
+def command_send_ans(m):
+    """
+    Обработка команды принятия ответа пользователя, пока пустая!!!!
+    """
+    id = m.from_user.id
+    bot.send_message(id, 'Cock and ball') 
+
+@bot.message_handler(commands=['help'])
+def command_help(m):
+    """
+    Обработка команды помощи пользователю
+    """
+    id = m.from_user.id
+    help_text = "The following commands are available: \n"
+    for key in commands:  
+        help_text += "/" + key + ": "
+        help_text += commands[key] + "\n"
+    bot.send_message(id, help_text)  
+
+
+#Start message handling
+bot.polling()
---
+++ room.py
@@ -0,0 +1,31 @@
+﻿class Room:
+    def __init__(self):
+        self.UID = -1 #Уникальный идентификатор комнаты, формат AAAA, 4 латинские буквы
+        self.Users = [] #Массив id пользователей данной комнаты
+    
+    def insert(self, x):
+        """
+        Вставка нового пользователя
+        """
+        self.Users.append(x)
+
+    def remove(self, x):
+        """
+        Удаление пользователя 
+        """
+        self.Users.remove(x)
+
+    def count(self):
+        """
+        Вернуть текущее количество пользователей
+        """
+        return len(self.Users)
+
+    def clear(self):
+        """
+        Очистить список пользователей
+        """
+        self.Users.clear()
+
+    def setUID(self, x):
+        self.UID = x
