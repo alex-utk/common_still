1ba78125465c7239149402ed3a9ff9be4e66b83f
fusted35 <72536481+fusted35@users.noreply.github.com>
2021-12-15 17:13:22 +0500

    TimerSprite

---
+++ gameclass.py
@@ -0,0 +1,61 @@
+import pygame
+import random
+import os
+import random
+
+
+class timerSprite(pygame.sprite.Sprite):
+    def __init__(self, Width = 100, Height = 100, x = 0, y = 0, fps = 30, on_stop = lambda x: None, text = ""):
+        pygame.sprite.Sprite.__init__(self)
+        self.tick = 0
+        self.fps = fps
+        self.image = pygame.Surface((Width - 100, 25))
+        self.surf = pygame.Surface((Width, Height))
+        self.surf.fill((169, 169, 169))
+        self.rect = self.surf.get_rect()
+        self.width = Width
+        self.height = Height
+        self.on = False
+        self.color = (0, 255, 0)
+        self.image.fill(self.color)
+        self.visible = True
+        self.x = x
+        self.y = y 
+        self.rect.center = (x, y)
+        self.interval = 0
+        self.on_stop = on_stop
+        (self.text_surface1, self.text_rect1) = draw_text(text, 50, self.x, self.y - Height / 4, "center")
+
+    def timerStart(self, interval, on_stop = lambda x: None):
+        self.interval = interval
+        self.on = True
+        self.tick = 0
+        self.on_stop = on_stop
+    
+    def update(self):
+        if not self.on:
+            return
+        cntTick = self.fps * self.interval
+        self.tick += 1
+        scale = (self.width - 100) * ( (cntTick - self.tick) / cntTick)
+        if scale > 1:
+            self.image = pygame.Surface((scale, 25))
+            self.image.fill(self.color)
+            txt = str(self.interval - self.tick // self.fps)
+            (self.text_surface2, self.text_rect2) = draw_text(txt, 24, self.width / 2, 3 * self.height / 4, "center")
+        else:
+            self.on = False
+            self.on_stop(self)
+
+    def draw(self, screen):
+        if not self.visible:
+            return
+        self.surf.fill((169, 169, 169))
+        rect = self.image.get_rect()
+        rect.center = (self.width / 2, 3 * self.height / 4 )
+        self.surf.blit(self.image, rect)
+        if self.on:
+            self.surf.blit(self.text_surface2, self.text_rect2)
+        screen.blit(self.surf, self.rect)
+        screen.blit(self.text_surface1, self.text_rect1)
+
