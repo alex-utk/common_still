745c45a35bc4e53ff6be4c608c4a3cd52b250ed5
EgorOfff <72536831+EgorOfff@users.noreply.github.com>
2021-12-16 01:25:59 +0500

    add new file

--- game_class_2.py
+++ game_class_2.py
@@ -31,4 +31,40 @@ class InputBox:
         self.active = False
         self.visible =True
 
-    
+def handle_event(self, event):
+        if not self.visible:
+            return
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            # If the user clicked on the input_box rect.
+            if self.rect.collidepoint(event.pos):
+                # Toggle the active variable.
+                self.active = not self.active
+            else:
+                self.active = False
+            # Change the current color of the input box.
+            self.color = self.COLOR_ACTIVE if self.active else self.COLOR_INACTIVE
+        if event.type == pygame.KEYDOWN:
+            if self.active:
+                if event.key == pygame.K_RETURN:
+                    print(self.text)
+                    self.text = ''
+                elif event.key == pygame.K_BACKSPACE:
+                    self.text = self.text[:-1]
+                else:
+                    self.text += event.unicode
+                # Re-render the text.
+                self.txt_surface = self.FONT.render(self.text, True, self.color)
+
+
+        def update(self):
+            self.txt_surface = self.FONT.render(self.text, True, self.color)
+            pass
+
+        def draw(self, screen):
+            if not self.visible:
+                return
+            # Blit the text.
+            screen.blit(self.txt_surface, (self.rect.x+self.w // 2, self.rect.y+self.h // 3))
+            # Blit the rect.
+            pygame.draw.rect(screen, self.color, self.rect, 2)
+
