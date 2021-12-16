0ed1130109877062b5121bd47a15f1848cb3ff7b
Alexandr Utkov <a.utkov@everypixel.ru>
2021-12-16 13:16:19 +0500

    added more complex examples

--- pygame_vid_example.py
+++ pygame_vid_example.py
@@ -22,3 +22,56 @@ while run:
     # set window title to current duration of video as hour:minute:second
     #t = video.current_time.format("%h:%m:%s")
     #pygame.display.set_caption(t)
+
+    # start video
+video.play()
+
+# main loop
+run = True
+while run:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT: # выход из игры
+            run = False
+    # draw video to display surface
+    # this function must be called every tick
+    video.draw_to(window, (0, 0))
+    pygame.display.update()
+
+    # set window title to current duration of video as hour:minute:second
+    #t = video.current_time.format("%h:%m:%s")
+    #pygame.display.set_caption(t)
+    # start video
+video.play()
+
+# main loop
+run = True
+while run:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT: # выход из игры
+            run = False
+    # draw video to display surface
+    # this function must be called every tick
+    video.draw_to(window, (0, 0))
+    pygame.display.update()
+
+    # set window title to current duration of video as hour:minute:second
+    #t = video.current_time.format("%h:%m:%s")
+    #pygame.display.set_caption(t)
+    # start video
+video.play()
+
+# main loop
+run = True
+while run:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT: # выход из игры
+            run = False
+    # draw video to display surface
+    # this function must be called every tick
+    video.draw_to(window, (0, 0))
+    pygame.display.update()
+
+    # set window title to current duration of video as hour:minute:second
+    #t = video.current_time.format("%h:%m:%s")
+    #pygame.display.set_caption(t)
+    
